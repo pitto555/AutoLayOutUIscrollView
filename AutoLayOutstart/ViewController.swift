111e9d3b3771a73a4583ba9a4b13e4af13ccd3e6
aoka <aoka.555.ibiza@gmail.com>
2021-05-02 19:34:44 +0900

    FirstCommit

---
+++ AutoLayOutstart/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AutoLayOutstart
+//
+//  Created by aoka on 2021/05/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button2: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
